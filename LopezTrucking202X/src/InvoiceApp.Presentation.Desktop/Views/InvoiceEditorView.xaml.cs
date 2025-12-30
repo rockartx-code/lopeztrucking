c92eca451dc8d8d813fb63f3e228164afe08cfeb
rockartx-code <rock.artx@gmail.com>
2025-12-30 07:56:40 -0600

    Add invoice editor view and view model

---
+++ LopezTrucking202X/src/InvoiceApp.Presentation.Desktop/Views/InvoiceEditorView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace InvoiceApp.Presentation.Desktop.Views;
+
+public partial class InvoiceEditorView : UserControl
+{
+    public InvoiceEditorView()
+    {
+        InitializeComponent();
+    }
+}
